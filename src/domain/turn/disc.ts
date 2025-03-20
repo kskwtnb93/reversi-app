11f99c76c0f3e56325cc3daa1d19f81c05d5692a
keisuke watanabe <kskwtnb93@gmail.com>
2025-03-20 19:41:30 +0900

    feat: 盤面に置けるかチェックする処理を実装

--- src/domain/turn/disc.ts
+++ src/domain/turn/disc.ts
@@ -6,6 +6,7 @@ export const Disc = {
   Empty: 0,
   Dark: 1,
   Light: 2,
+  Wall: 3,
 } as const
 
 export type Disc = (typeof Disc)[keyof typeof Disc]
@@ -13,4 +14,14 @@ export type Disc = (typeof Disc)[keyof typeof Disc]
 export function toDisc(value: number): Disc {
   // TODO: あとで改善
   return value as Disc
+}
+
+/**
+ * 逆の色の石かを判定する
+ */
+export function isOppositeDisc(disc1: Disc, disc2: Disc): boolean {
+  return (
+    (disc1 === Disc.Dark && disc2 === Disc.Light) ||
+    (disc1 === Disc.Light && disc2 === Disc.Dark)
+  )
 }
